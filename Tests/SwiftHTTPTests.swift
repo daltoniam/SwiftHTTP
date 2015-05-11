4955cc7f6a9118a347dfa9ec2b6cbca5a06c7651
Robert M. Ryan <robert.ryan@mindspring.com>
2015-05-11 12:09:22 -0400

    Add dependencies test
    The libraries was not handling operations properly, so created test to illustrate the problem. Then we'll fix the problem and see the test resolved.
    
    Also added a missing wait that I neglected to include in last commit.

--- Tests/SwiftHTTPTests.swift
+++ Tests/SwiftHTTPTests.swift
@@ -59,5 +59,46 @@ class SwiftHTTPTests: XCTestCase {
             XCTAssert(false, "Failure")
             expectation.fulfill()
         })
+        
+        waitForExpectationsWithTimeout(30, handler: nil)
+    }
+    
+    func testOperationDependencies() {
+        let expectation1 = expectationWithDescription("testOperationDependencies1")
+        let expectation2 = expectationWithDescription("testOperationDependencies2")
+        
+        let operationQueue = NSOperationQueue()
+        operationQueue.maxConcurrentOperationCount = 2
+        
+        var operation1Finished = false
+        
+        let urlString1 = "http://photojournal.jpl.nasa.gov/tiff/PIA19330.tif" // (4.32 MB)
+        let urlString2 = "http://photojournal.jpl.nasa.gov/jpeg/PIA19330.jpg" // (0.14 MB)
+        
+        let request1 = HTTPTask()
+        let op1 = request1.create(urlString1, method: .GET, parameters: nil, success: { (response) -> Void in
+            operation1Finished = true
+            expectation1.fulfill()
+        }) { (error, response) -> Void in
+            operation1Finished = true
+            XCTFail("request1 failed: \(error.localizedDescription)")
+            expectation1.fulfill()
+        }
+        
+        let request2 = HTTPTask()
+        let op2 = request2.create(urlString2, method: .GET, parameters: nil, success: { (response) -> Void in
+            XCTAssert(operation1Finished, "Operation 1 did not finish first")
+            expectation2.fulfill()
+        }) { (error, response) -> Void in
+            XCTFail("request1 failed: \(error.localizedDescription)")
+            XCTAssert(operation1Finished, "Operation 1 did not finish first")
+            expectation2.fulfill()
+        }
+        
+        op2?.addDependency(op1!)
+        operationQueue.addOperation(op1!)
+        operationQueue.addOperation(op2!)
+        
+        waitForExpectationsWithTimeout(30, handler: nil)
     }
 }
